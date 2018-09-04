6e3e61f4b9f2b11ec6ef21c248ed95b20ab0d832
nay man <nayman.po@gmail.com>
2018-09-04 09:30:24 +0700

    create login

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,12 @@ using DatingApp.API.Data;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using DatingApp.API.Dtos;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System;
+
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
@@ -36,6 +42,34 @@ namespace DatingApp.API.Controllers
             var createUser = await _repo.Register(userToCreate, userdto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserForLoginDto userlogindto){
+            var userFromRepo = await _repo.Login(userlogindto.Username.ToLower(),userlogindto.Password);
+            if( userFromRepo == null ){
+                return Unauthorized();
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes("super secret Key");
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim( ClaimTypes.NameIdentifier , userFromRepo.Id.ToString()),
+                    new Claim( ClaimTypes.Name , userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken( token );
+            return Ok(tokenString);
         }
     }
 }
